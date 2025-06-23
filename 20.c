084b8e26045e803919b580e47d8dbfa32ea1eba6
KHUBAIB <khubaibnaeem42@gmail.com>
2025-06-24 02:50:46 +0500

    CHECK IF NUMBER IS EVEN OR ODD

---
+++ 20.c
@@ -0,0 +1,17 @@
+// CREATE A PROGRAM THAT DETERMINES IF A NUMBER IS ODD OR EVEN//
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    if (number % 2 == 0)
+    {
+        printf("%d is even.\n", number);
+    }
+    else
+    {
+        printf("%d is odd.\n", number);
+    }
+    return 0;
+}
