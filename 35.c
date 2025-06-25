131bff9accbcd1c9489539bec301adb694def796
KHUBAIB <khubaibnaeem42@gmail.com>
2025-06-25 13:27:10 +0500

    LCM

---
+++ 35.c
@@ -0,0 +1,27 @@
+// CREATE A PROGRAM TO FIND THE LEAST COMMON MULTIPLE OF TWO NUMBERS //
+#include <stdio.h>
+int main()
+{
+    int num1, num2, lcm;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    int min = (num1 > num2) ? num1 : num2;
+    int max = num1 * num2;
+
+    for (int i = min; i <= max; i++)
+    {
+        if (i % num1 == 0 && i % num2 == 0)
+        {
+            lcm = i;
+            break;
+        }
+    }
+    printf("The LCM of %d and %d is %d\n", num1, num2, lcm);
+
+    return 0;
+}
+
+// 12, 13
