c1dd8a2fc3f72d637d8e2acd327f5272accd210b
KHUBAIB <khubaibnaeem42@gmail.com>
2025-06-26 13:10:06 +0500

    PATTERNS

---
+++ 01.c
@@ -0,0 +1,31 @@
+// SHOW THE FOLLOWING PATTERN JUST USING PRINT STATEMENT//
+
+#include <stdio.h>
+
+int main()
+{
+
+	// RIGHT HALF PYRAMID//
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("*****\n");
+
+	printf("\n");
+	// REVERSE RIGHT HALF PYRAMID//
+	printf("*****\n");
+	printf("****\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*\n");
+
+	printf("\n");
+	// LEFT HALF PYRAMID//
+	printf("    *\n");
+	printf("   **\n");
+	printf("  ***\n");
+	printf(" ****\n");
+	printf("*****\n");
+	return 0;
+}
