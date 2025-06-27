d0f0d60fe7ee44110658e8eaac66c9578b66c666
KHUBAIB <khubaibnaeem42@gmail.com>
2025-06-27 18:23:29 +0500

    Add greet function to print 'Hello World!' when called

---
+++ 50.c
@@ -0,0 +1,20 @@
+// WRITE A FUNCTION NAMED GREET THAT PRINT "HELLO WORLD!" WHEN CALLED //
+
+#include <stdio.h>
+
+void greet();
+
+int main()
+{
+    greet();
+    return 0;
+}
+
+void greet()
+{
+    printf("Hello World!");
+}
+
+void h()
+{
+}
