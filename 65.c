a59a1a8cb6b7dbc155092012d59e971c69193ed3
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-01 03:06:33 +0500

    Demonstrate overflow and wraparound of signed and unsigned int types

---
+++ 65.c
@@ -0,0 +1,13 @@
+/*WRITE A PROGRAM THAT INITIALIZES AN UNSIGNED INT TO ITS MAXIMUM POSSIBLE VALUE AND AN INTEGER TO A NEGATIVE NUMBER ADD 1 TO BOTH AND PRINT THE RESULT TO SHOW HOW THE UNSIGNED INT WRAP
+AROUND TO 0, WHERE AS THE INT REMAIN NEGATIVE DUE TO OVERFLOW*/
+
+#include <stdio.h>
+int main()
+{
+    unsigned int num1 = 4294967295;
+    int num2 = 2147483647;
+
+    printf("int = %d\n", num2 + 1);
+    printf("unsigned int = %u\n", num1 + 1);
+    return 0;
+}
