a046272a83ca0757fb04877fc911370b5bbf9186
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-02 01:10:31 +0500

    program to read a line using fgets and print it with puts

---
+++ 78.c
@@ -0,0 +1,13 @@
+// READ A LINE OF TEXT FROM THE USER USING FGETS AND THEN PRINT IT USING PUTS //
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char text[200];
+    printf("Enter text: ");
+    fgets(text, 200, stdin);
+    puts(text);
+    return 0;
+}
