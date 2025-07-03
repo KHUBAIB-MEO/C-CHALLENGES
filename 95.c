8404d6f03e074271ead9dc44f71c9995ff77c96b
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-03 16:44:02 +0500

    Implement dynamic float array allocation using malloc with user input, validation, and output display.

---
+++ 95.c
@@ -0,0 +1,34 @@
+// CREATE A PROGRAM THAT USE MALLOC TO DYNAMICALLY ALLOCATE AN ARRAY FOR A SPECIFIED NUMBER OF FLOAT VALUE ENTERED BY THE USER AND THEN STORE USER ENTERED NUMBER INTO IT //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Enter size of array: ");
+    scanf("%d", &num);
+
+    float *arr = (float *)malloc(num * sizeof(float));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("Enter %d number: ", i + 1);
+        scanf("%f", &arr[i]);
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("%d number is %.2f: \n", i + 1, arr[i]);
+    }
+
+    free(arr);
+    arr = NULL;
+    return 0;
+}
