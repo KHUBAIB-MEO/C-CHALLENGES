bd82fb3ff82d493eda7114c075bb43531a6f7406
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-04 02:52:42 +0500

    Add dynamic memory allocation for 2D points with user input and cleanup

---
+++ 97.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x_coordinate;
+    int y_coordinate;
+} Point;
+
+int main()
+{
+    Point *start = (Point *)malloc(sizeof(Point));
+    Point *end = (Point *)malloc(sizeof(Point));
+    if (start == NULL || end == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    printf("Enter start X coordinate: ");
+    scanf("%d", &start->x_coordinate);
+    printf("Enter start Y coordinate: ");
+    scanf("%d", &start->y_coordinate);
+    printf("Enter end X coordinate: ");
+    scanf("%d", &end->x_coordinate);
+    printf("Enter end Y coordinate: ");
+    scanf("%d", &end->y_coordinate);
+
+    printf("Start x and y coordinate : (%d,%d)\n", start->x_coordinate, start->y_coordinate);
+    printf("End x and y coordinate : (%d,%d)", end->x_coordinate, end->y_coordinate);
+
+    free(start);
+    free(end);
+
+    start = NULL;
+    end = NULL;
+    return 0;
+}
