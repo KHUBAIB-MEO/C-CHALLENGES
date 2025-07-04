18969168f9b8a5355cde7c7c1256fbc4a69daa0b
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-05 02:37:16 +0500

    Add file-based integer sum calculator

---
+++ 100.c
@@ -0,0 +1,40 @@
+// CREATE A PROGRAM THAT READ INT FROM FILE AND CALCULATE ITS SUM//
+
+#include <stdio.h>
+int main()
+{
+    int size;
+    FILE *sum = fopen("sum.txt", "w+");
+    if (sum == NULL)
+    {
+        printf("File can't create\n");
+        return 1;
+    }
+    printf("Enter how many number you want to enter: ");
+    scanf(" %d", &size);
+    int arr[size];
+    for (int j = 0; j < size; j++)
+    {
+        printf("Enter %d element: ", j + 1);
+        scanf("%d", &arr[j]);
+    }
+    for (int j = 0; j < size; j++)
+    {
+        fprintf(sum, "%d\n", arr[j]);
+    }
+    rewind(sum);
+
+    int number;
+    int i = 0;
+    int numberSum = 0;
+    while (fscanf(sum, "%d", &number) == 1)
+    {
+        numberSum = numberSum + number;
+        i++;
+    }
+    printf("The sum of number are: %d", numberSum);
+
+    fclose(sum);
+
+    return 0;
+}
