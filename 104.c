e32fb1b6f8d6a77eb8161f320bef4b3681f6b20f
KHUBAIB <khubaibnaeem42@gmail.com>
2025-07-06 02:26:26 +0500

    Append text to existing file using r+ mode and fseek

--- 104.c
+++ 104.c
@@ -3,38 +3,15 @@
 #include <stdio.h>
 int main()
 {
-    int size;
-    FILE *sum = fopen("data.txt", "w+");
-    if (sum == NULL)
+    FILE *file = fopen("data.txt", "r+");
+    if (file == NULL)
     {
         printf("File can't create\n");
         return 1;
     }
-    printf("Enter how many number you want to enter: ");
-    scanf("%d", &size);
-    int arr[size];
-    for (int j = 0; j < size; j++)
-    {
-        printf("Enter %d element: ", j + 1);
-        scanf("%d", &arr[j]);
-    }
-    for (int j = 0; j < size; j++)
-    {
-        fprintf(sum, "%d\n", arr[j]);
-    }
-    rewind(sum);
-
-    int number;
-    int i = 0;
-    int numberSum = 0;
-    while (fscanf(sum, "%d", &number) == 1)
-    {
-        numberSum = numberSum + number;
-        i++;
-    }
-    printf("The sum of numbers is: %d\n", numberSum);
-
-    fclose(sum);
+    fseek(file, 0, SEEK_END);
+    fputs("\nHow are you", file);
+    fclose(file);
 
     return 0;
 }
